03db709f8f5eeebb7b30223ae1d9862602eac00f
halewalls2026 <143747870+halewalls2026@users.noreply.github.com>
2023-12-05 20:15:57 -0500

    Update SearchAccount.h

--- SearchAccount.h
+++ SearchAccount.h
@@ -5,6 +5,6 @@
 #ifndef FINAL_SEARCH_ACCOUNT_H
 #define FINAL_SEARCH_ACCOUNT_H
 
-void SearchAccount();
+void SearchAccount(string accountinfo[][5], int numAccounts);
 
 #endif //FINAL_SEARCH_ACCOUNT_H
