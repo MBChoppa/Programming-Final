c3ce9e85fc4ee245a6d162a42eb7cd637b0675dc
halewalls2026 <143747870+halewalls2026@users.noreply.github.com>
2023-12-05 20:49:20 -0500

    Create CheckAccountBalance.h

---
+++ CheckAccountBalance.h
@@ -0,0 +1,10 @@
+//
+// Created by Hale Walls on 12/5/23
+//
+
+#ifndef FINAL_CHECK_ACCOUNT_BALANCE_H
+#define FINAL_CHECK_ACCOUNT_BALANCE_H
+
+void CheckAccountBalance(string accountinfo[][5], accountfiles[], int numAccounts);
+
+#endif //FINAL_CHECK_ACCOUNT_BALANCE_H
