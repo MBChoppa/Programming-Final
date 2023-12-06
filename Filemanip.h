d24f477201d912aef5d126c5db48b4b6c5687f71
halewalls2026 <143747870+halewalls2026@users.noreply.github.com>
2023-12-05 21:07:13 -0500

    Create Filemanip.h

---
+++ Filemanip.h
@@ -0,0 +1,5 @@
+
+
+const string TELLERS = "tellers.dat", ACCOUNTS = "accounts.dat"; //defining file names.
+
+void filemanip(string tellerinfo[][2], int numTellers, string accountinfo[][5], int numAccounts); 
