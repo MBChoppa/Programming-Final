c6280e033faba5fb437bf602ff528f32c35f382e
halewalls2026 <143747870+halewalls2026@users.noreply.github.com>
2023-12-05 21:09:27 -0500

    Update Filemanip.h

--- Filemanip.h
+++ Filemanip.h
@@ -1,5 +1,12 @@
+//
+// Created by Hale Walls on 12/5/23
+//
 
+#ifndef FILEMANIP_H
+#define FILEMANIP_H
 
 const string TELLERS = "tellers.dat", ACCOUNTS = "accounts.dat"; //defining file names.
 
 void filemanip(string tellerinfo[][2], int numTellers, string accountinfo[][5], int numAccounts); 
+
+#endif //FILEMANIP_H
