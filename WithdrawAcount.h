ee77c3d6b4d915961983ed670f105a0a8549e2f2
halewalls2026 <143747870+halewalls2026@users.noreply.github.com>
2023-12-05 20:41:31 -0500

    Create WithdrawAcount.h

---
+++ WithdrawAcount.h
@@ -0,0 +1,10 @@
+//
+// Created by Hale Walls on 12/5/23
+//
+
+#ifndef FINAL_WITHDRAW_ACCOUNT_H
+#define FINAL_WITHDRAW_ACCOUNT_H
+
+void WithdrawAccount(string accountinfo[][5], accountfiles[], int numAccounts);
+
+#endif //FINAL_WITHDRAW_ACCOUNT_H
