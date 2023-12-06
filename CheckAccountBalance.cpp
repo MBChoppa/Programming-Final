8e24ee9edf2995afa28d889cd629ef05c0d36b40
halewalls2026 <143747870+halewalls2026@users.noreply.github.com>
2023-12-05 20:48:10 -0500

    Create CheckAccountBalance.cpp

---
+++ CheckAccountBalance.cpp
@@ -0,0 +1,35 @@
+//
+// Create by Hale Walls on 12/5/23
+//
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+void CheckAcountBalance(string accountinfo[][5], string accountfiles[], int numAccounts)
+{
+    cout << "Please enter the account number: "; 
+    string num;
+    int place = -1; //set up for checking validitiy. 
+    cin >> num;
+
+    for (int i = 0; i < numAccounts; i++) //checks to see if the inputed number is valid.
+    {
+        if (accountinfo[i][0] == num)
+        {
+            place = i;
+        }
+    }
+    if (place == -1)
+    {
+        cout << "That is not an account that exists." << endl;
+        return;
+    }
+    float balance;
+    ifstream fin;
+    fin.open(accountfiles[place]); //opens the account file.
+    fin >> balance; //gets the balance number.
+    fin.close();
+    cout << "The balance for that account is $" << balance << "." << endl; //prints the balance currently in the account.
+
+}
