5e0e1a5e8812357678964c29d24edb85aeb013f2
halewalls2026 <143747870+halewalls2026@users.noreply.github.com>
2023-12-05 19:59:18 -0500

    Update SearchAccount.cpp

--- SearchAccount.cpp
+++ SearchAccount.cpp
@@ -1,22 +1,25 @@
-cout << "Please enter the account number." << endl; //account number is the name of the file it will be drawn from.
-string num;
-int place = -1; //set up for checking validitiy. 
-cin >> num;
+void SearchAccount()
+{
+    cout << "Please enter the account number." << endl; //account number is the name of the file it will be drawn from.
+    string num;
+    int place = -1; //set up for checking validitiy. 
+    cin >> num;
 
-for (int i = 0; i < 19; i++) //checks to see if the inputed number is valid.
-{
-    if (accountinfo[i][0] == num)
+    for (int i = 0; i < 19; i++) //checks to see if the inputed number is valid.
     {
-        place = i;
+        if (accountinfo[i][0] == num)
+        {
+            place = i;
+        }
     }
-}
-if (place == -1)
-{
-    cout << "That is not an account that exists." << endl;
-    break;
-}
+    if (place == -1)
+    {
+        cout << "That is not an account that exists." << endl;
+        break;
+    }
     cout << "Here is the information for account number " << accountinfo[place][0] << endl; //prints the account information.
     for (int i = 1; i < 5; i++) 
     {
         cout << accountinfo[place][i] << endl;
     }
+}
