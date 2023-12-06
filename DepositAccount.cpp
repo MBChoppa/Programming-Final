d68f35e51460ed50a0960e704e37bb7cfa5811b0
halewalls2026 <143747870+halewalls2026@users.noreply.github.com>
2023-12-05 20:22:29 -0500

    Create DepositAccount.cpp

---
+++ DepositAccount.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void DepositAccount(string accountinfo[][5], int numAccounts)
+{
+    string filename = "account.dat";
+    cout << "Please enter the account number: ";
+    string num;
+    int place = -1; //set up for checking validitiy. 
+    cin >> num;
+
+    for (int i = 0; i < 19; i++) //checks to see if the inputed number is valid.
+    {
+        if (accountinfo[i][0] == num)
+        {
+            place = i;
+        }
+    }
+    if (place == -1)
+    {
+        cout << "That is not an account that exists." << endl;
+        return;
+    }
+    float deposit = 0.00; //using float because of decimal places.
+    cout << "How much money would you like to deposit into the account?" << endl; 
+    cin >> deposit; //gets amount.
+
+    if(deposit >= 0) //checks to see if the amount is valid. 
+    {
+        ofstream fout; 
+        fout.open(accountfiles[place]); //opens the file for the account.
+        fout << deposit; //deposits the amount into the file.
+        fout.close();
+        cout << "Depositing $" << deposit << " into that account." << endl; 
+    }
+    else
+    {
+        cout << "That is not a valid amount." << endl;
+        return;
+    }
+}
