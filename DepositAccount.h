45ec90143a7531d93da0f0f79cc839105850fc80
halewalls2026 <143747870+halewalls2026@users.noreply.github.com>
2023-12-05 20:26:56 -0500

    Create DepositAccount.h

---
+++ DepositAccount.h
@@ -0,0 +1,10 @@
+//
+// Created by Hale Walls on 12/5/23
+//
+
+#ifndef FINAL_DEPOSIT_ACCOUNT_H
+#define FINAL_DEPOSIT_ACCOUNT_H
+
+void DepositAccount(string accountinfo[][5], int numAccounts);
+
+#endif //FINAL_DEPOSIT_ACCOUNT_H
