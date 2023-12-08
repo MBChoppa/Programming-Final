9218ba625e27e1cfbc5389f0be966720bbbf5875
Evan Crabtree <130472137+MBChoppa@users.noreply.github.com>
2023-12-08 16:42:32 -0500

    Update Filemanip.h
    adding changes made after testing in clion

--- Filemanip.h
+++ Filemanip.h
@@ -1,7 +1,6 @@
 //
 // Created by Hale Walls on 12/5/23
 //
-
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -11,6 +10,6 @@
 
 const std::string TELLERS = "tellers.dat", ACCOUNTS = "accounts.dat"; //defining file names.
 
-void filemanip(std::string tellerinfo[][2], int numTellers, std::string accountinfo[][5], int numAccounts);
+void filemanip(std::string tellerinfo[][2], int numTellers, std::string accountinfo[][5], std::string accountfiles[], int numAccounts);
 
 #endif //FILEMANIP_H
