688347c4f45314b3b78ded53d5a0edad02caef7e
Evan Crabtree <130472137+MBChoppa@users.noreply.github.com>
2023-12-08 14:42:30 -0500

    Update Filemanip.h

--- Filemanip.h
+++ Filemanip.h
@@ -2,11 +2,15 @@
 // Created by Hale Walls on 12/5/23
 //
 
+#include <iostream>
+#include <string>
+#include <fstream>
+
 #ifndef FILEMANIP_H
 #define FILEMANIP_H
 
-const string TELLERS = "tellers.dat", ACCOUNTS = "accounts.dat"; //defining file names.
+const std::string TELLERS = "tellers.dat", ACCOUNTS = "accounts.dat"; //defining file names.
 
-void filemanip(string tellerinfo[][2], int numTellers, string accountinfo[][5], int numAccounts); 
+void filemanip(std::string tellerinfo[][2], int numTellers, std::string accountinfo[][5], int numAccounts);
 
 #endif //FILEMANIP_H
