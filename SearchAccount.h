63aeb7e06b48b6412573a6dff2bef0b7792784eb
Evan Crabtree <130472137+MBChoppa@users.noreply.github.com>
2023-12-08 14:44:27 -0500

    Update SearchAccount.h

--- SearchAccount.h
+++ SearchAccount.h
@@ -2,9 +2,13 @@
 // Created by Hale Walls on 12/5/23
 //
 
+#include <iostream>
+#include <string>
+#include <fstream>
+
 #ifndef FINAL_SEARCH_ACCOUNT_H
 #define FINAL_SEARCH_ACCOUNT_H
 
-void SearchAccount(string accountinfo[][5], int numAccounts);
+void SearchAccount(std::string accountinfo[][5], int numAccounts);
 
 #endif //FINAL_SEARCH_ACCOUNT_H
