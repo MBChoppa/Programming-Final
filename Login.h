688593b417be0f2a19d74dc679385e208b73ac50
Evan Crabtree <130472137+MBChoppa@users.noreply.github.com>
2023-12-08 16:49:45 -0500

    Update Login.h
    adding changes made after testing in clion

--- Login.h
+++ Login.h
@@ -1,10 +1,13 @@
 //
 // Created by Hale Walls on 12/5/23
 //
+#include <iostream>
+#include <string>
+#include <fstream>
 
 #ifndef LOGIN_H
 #define LOGIN_H
 
-void login(string tellerinfo[][2], int numTellers);
+void login(std::string tellerinfo[][2], int numTellers);
 
 #endif //LOGIN_H
